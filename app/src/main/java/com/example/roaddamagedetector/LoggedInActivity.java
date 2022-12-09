2530933004a804aac2aec3f287d6197452f8b60d
Jimson Ejaus <carl.ejaus@obf.ateneo.edu>
2022-12-10 01:24:28 +0800

    Added NavBar

--- app/src/main/java/com/example/roaddamagedetector/LoggedInActivity.java
+++ app/src/main/java/com/example/roaddamagedetector/LoggedInActivity.java
@@ -1,13 +1,18 @@
 package com.example.roaddamagedetector;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
@@ -30,6 +35,9 @@ public class LoggedInActivity extends AppCompatActivity {
 
     @ViewById
     ImageView imageViewOnLogIn;
+
+    @ViewById
+    BottomNavigationView bottom_navigation;
 
     SharedPreferences prefs;
     Realm realm;
@@ -61,5 +69,30 @@ public class LoggedInActivity extends AppCompatActivity {
         }
 
         tvOutput.setText(outputText);
+        bottom_navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                Activity activity = null;
+                Intent myIntent;
+                switch (item.getItemId()){
+
+                    case R.id.nav_dash:
+                        myIntent = new Intent(LoggedInActivity.this, DashboardActivity_.class);
+                        myIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(myIntent);
+//                        DashboardActivity_.intent(MainActivity.this).start();
+//                        finish();
+                        break;
+                    case R.id.nav_settings:
+                        myIntent = new Intent(LoggedInActivity.this, SettingsActivity_.class);
+                        myIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(myIntent);
+//                        SettingsActivity_.intent(MainActivity.this).start();
+//                        finish();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
